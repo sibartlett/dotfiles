28c7885a9abb6f55d03e7f814c30bcdb607fb491
Simon Bartlett <simon@sibartlett.com>
2016-11-22 13:23:38 -0500

    Fixes

--- roles/zsh/_path.zsh
+++ roles/zsh/_path.zsh
@@ -1,3 +1,3 @@
 # Customize to your needs...
 # Make sure to APPEND to the path, so that homebrewed binaries, e.g. /usr/local/bin are found first
-export PATH=${PATH}:$ZSH/bin
+export PATH="/usr/local/sbin:$PATH:$ZSH/bin"
